be7fe0a6719c02996b7bb9d98bd0c7265f4e6dad
Joey <JTFunny5000@gmail.com>
2015-04-15 14:22:52 -0400

    edited readme

--- FFT.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python3
-
-from numpy.fft import fft
-from numpy import array
-
-a = array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0])
-print( ' '.join("%5.3f" % abs(f) for f in fft(a)) )
